bf14138477fd11a42f72e54d52194a79ba44a1a8
leandroPorcelli1 <leandroporcelli657@gmail.com>
2023-01-17 19:36:12 +0100

    creando primer componente en react

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import ContadorApp from "./contadorApp";
+
+// const datos = { nombre: "leandro", apellido: "Porcelli" };
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ContadorApp value={1234} />
+  </React.StrictMode>
+);
